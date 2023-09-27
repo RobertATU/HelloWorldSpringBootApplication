eaa13077cadd13b4a23fb3aefe2fc74a8764c6fa
Robert <G00395612@atu.ie>
2023-09-27 10:11:53 +0100

    First Commit

---
+++ src/test/java/ie/atu/helloworldspringbootapplication2/HelloWorldSpringBootApplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.helloworldspringbootapplication2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBootApplication2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
